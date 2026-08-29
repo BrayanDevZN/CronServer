e1806fae310cbfe9978f63c5cd2f082c500ae391
Brayan <lsouzadiasvaz@gmail.com>
2026-08-29 12:22:20 -0300

    feat: create connection db

--- src/infra/manage.py
+++ src/infra/manage.py
@@ -8,3 +8,10 @@ Junta os modulos de infra e facilita a importação
 from src.infra.core.settings  import ConfigEnviroin
 config = ConfigEnviroin()
 envroins = config.get()
+
+
+#Junta a classe que faz conexão com banco de dados com a variavel de ambiente requerida
+from src.infra.connect.db import ConnectDb
+engine = ConnectDb(url=envroins["url"]).run()
+
+
