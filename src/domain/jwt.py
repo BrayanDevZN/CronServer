6d909b152e3aa15d553e903085fb6b563388c413
Brayan <lsouzadiasvaz@gmail.com>
2026-08-29 22:33:42 -0300

    feat: jwt domain

---
+++ src/domain/jwt.py
@@ -0,0 +1,61 @@
+from src.logs.log import LogLayer
+logger = LogLayer("domain_jwt").config().logger()
+
+
+"""
+Cria e le os tokens jwt
+"""
+
+import jwt
+
+class JwtAuthError(Exception):
+    pass
+
+
+class JwtAuth:
+
+    def __init__(self, sing:str)-> None:
+
+        self.sing = sing
+        self.alg = "SH256"
+
+
+    #Cria o token
+    def create(self, payload:dict) -> str:
+
+        try: 
+
+            logger.info("Criando token...")
+
+            return jwt.encode(
+                payload=payload,
+                algorithm=self.alg,
+                key=self.sing
+            )
+
+        except Exception as e:
+
+            logger.error(e)
+            raise JwtAuthError(e)
+
+
+    #Le o token e retorna o payload
+    def read(self, token:str) -> dict:
+
+        try:  
+
+            logger.info("Lendo token...")
+
+            return jwt.decode(
+                algorithms=[self.alg],
+                key=self.sing,
+                jwt=token
+            )
+
+        except Exception as e:
+
+            logger.error(e)
+            raise JwtAuthError(e)
+
+
+        
